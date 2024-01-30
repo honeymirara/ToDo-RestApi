5b20ed3c7924314f924aee3c5e8704fc8478bf54
honeymirara <honeymirara@gmail.com>
2024-01-30 19:41:38 +0300

    add ENV Eslint Prettier + make swagger

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('../src/controller/task.controller')
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
 
 
 const app = express()
 
 app.use(bodyParser.json())
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 app.use('/task', route)
+
 
 
 app.use((err, req, res, next) => res.send(err.message))
