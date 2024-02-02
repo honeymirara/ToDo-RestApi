a1bc881a67c4b1421c9cf043df32273f3527e949
honeymirara <honeymirara@gmail.com>
2024-02-02 13:08:54 -1000

    add client-side

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import Main from './page/Main/Main'
+
+function App() {
+
+
+  return (
+    <div>
+      <Main />
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/page/Main/Main.tsx
@@ -0,0 +1,14 @@
+import style from './style.module.scss';
+
+export default function Main() {
+    return (
+        <div className={style.wrapper}>
+            <h1>TODO LIST</h1>
+            <div className={style.header}>
+                <input type="text" placeholder='enter note...'/>
+                <input type="text" placeholder='enter description note...'/>
+                <div><button>CREATE</button></div>
+            </div>
+        </div>
+    )
+}
